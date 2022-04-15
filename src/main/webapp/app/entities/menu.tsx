b55b23f5eda47899f35e1a4d8ef69752b070e33b
Eve Poitevin <poitevineve@gmail.com>
2022-04-15 08:31:13 +0200

    jhipster app

--- src/main/webapp/app/entities/menu.tsx
+++ src/main/webapp/app/entities/menu.tsx
@@ -7,6 +7,24 @@ const EntitiesMenu = () => {
   return (
     <>
       {/* prettier-ignore */}
+      <MenuItem icon="asterisk" to="/client">
+        <Translate contentKey="global.menu.entities.client" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/cooperative">
+        <Translate contentKey="global.menu.entities.cooperative" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/driver">
+        <Translate contentKey="global.menu.entities.driver" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/shop">
+        <Translate contentKey="global.menu.entities.shop" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/cart">
+        <Translate contentKey="global.menu.entities.cart" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/command">
+        <Translate contentKey="global.menu.entities.command" />
+      </MenuItem>
       {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
     </>
   );
